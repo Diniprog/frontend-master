f46abb3fdf0fa0a86f53e3d06edeab0b95f7b73e
Elvedin Sadiku <naniprintelvedin@gmail.com>
2023-11-17 20:40:51 +0100

    JAVA4

---
+++ java-4/script.js
@@ -0,0 +1,37 @@
+// // ##### Objektet dhe Arrayt - Përsëritje (#1) #####a
+// let produkti = {
+//  emri: "iPhone 13",
+//   pershkrimi: "iPhone 13 Pro",
+//   cmimi: 50
+// };
+// produkti.tags = ['Telefon', 'iphone' 'apple'];
+
+
+
+// ##### For loops - Praktikë (#2) #####
+// let teNgjashme = [“rezultante”, “rezulton”, “rezolutë”, “rezil”, “rezmishta”, “rezervuar”, “reze”];
+
+// for (let i = 0; i < 10; i++ ) {
+//     console.log(i + 1);
+// }
+
+
+// ##### For loops - Praktikë (#3) #####
+
+
+// ##### Array Iteration: Praktike (#4) #####
+const studentet = ['Aldin', 'Ali', 'Almir', 'Arlind', 'Artan', 'Elvedin', 'Gafar', 'Jon'];
+ for (let i = 0; i< 7; i++ ){
+ console.log(`Studentet :`, studentet[i]);
+};
+
+// ##### While Loop - Praktike (#5) #####
+
+
+
+// ##### While Loop - Praktike (#6) #####
+// nderkohe qe numri nuk eshte i sakte
+//     printo ne konzole mesazhin per te provuar perseri
+//     shfaq prompt-in perseri
+// perndryshe
+//     printo ne konsole mesazhin e suksesit
